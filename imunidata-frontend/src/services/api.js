69fe06d24f8955f1faf911d84788461b93ad873d
Gabriel Pardo <136826784+GabrieIPardo@users.noreply.github.com>
2026-05-14 23:23:49 -0300

    Set base URL for API requests

--- imunidata-frontend/src/services/api.js
+++ imunidata-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: 'https://imunidata-back.onrender.com/',
   headers: { 'Content-Type': 'application/json' },
 });
 
